67858b8cc3ffbe9baac96dc3c7085c36da2d69cf
ROJINS <rojins0209@gmail.com>
2026-01-18 00:46:30 +0530

    feat: Project Reorganization & Fixes (Exclude node_modules)

--- server/dist/config/supabase.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.supabase = void 0;
-const supabase_js_1 = require("@supabase/supabase-js");
-const env_1 = require("./env");
-if (!env_1.config.supabase.url || !env_1.config.supabase.key) {
-    throw new Error('Missing Supabase credentials in .env');
-}
-exports.supabase = (0, supabase_js_1.createClient)(env_1.config.supabase.url, env_1.config.supabase.key);
